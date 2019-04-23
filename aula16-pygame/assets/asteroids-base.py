df7cfd95f71cac6353d512704b5338be2f2bbb3a
Juan Ruiz <Juan@Juans-MacBook-Pro.local>
2019-04-23 17:46:44 -0300

    Parte 2 Completa

--- aula16-pygame/assets/asteroids-base.py
+++ aula16-pygame/assets/asteroids-base.py
@@ -3,9 +3,12 @@
 # Importando as bibliotecas necessárias.
 import pygame
 from os import path
+import random
+import time
 
 # Estabelece a pasta que contem as figuras.
 img_dir = path.join(path.dirname(__file__), 'img')
+snd_dir = path.join(path.dirname(__file__), 'snd')
 
 # Dados gerais do jogo.
 WIDTH = 480 # Largura da tela
@@ -45,6 +48,52 @@ class Player(pygame.sprite.Sprite):
         #centraliza embaixo da tela
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
+        
+        #vel da nave
+        self.speedx = 0
+        
+        #melhora colisao estabelecendo um raio de um circulo
+        self.radius = 25
+        
+    #metodo que atualiza a posicao da navinha
+    def update(self):
+        self.rect.x += self.speedx
+        
+        #mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
+class Mob(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        
+        pygame.sprite.Sprite.__init__(self)
+        
+        mob_img = pygame.image.load(path.join(img_dir, "meteorBrown_med1.png"))
+        self.image = mob_img
+        
+        self.rect = self.image.get_rect()
+        
+        self.rect.x = random.randrange(0,WIDTH)
+        self.rect.y = random.randrange(-100,-40)
+        
+        self.speedx = random.randrange(-3,3)
+        self.speedy = random.randrange(2,9)
+        
+        #melhora colisao estabelecendo um raio de um circulo
+        self.radius = int(self.rect.width * .85 / 2)
+        
+    #Metodo que atualiza a posicao do meteoro
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        
+
+        
+
+        
     
 
 # Inicialização do Pygame.
@@ -64,17 +113,33 @@ clock = pygame.time.Clock()
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
 
+#carrega os sons do jogo
+pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.set_volume(0.4)
+boom_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl3.wav'))
+
 #cria uma nave. O construtor sera chamado automaticamente
 player = Player()
+
 
 #cria um grupo de sprites e adiciona a nave
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
+all_mobs = pygame.sprite.Group()
+
+i=0
+while i < 8:
+    mob = Mob()
+    all_mobs.add(mob)
+    all_sprites.add(mob)
+    i+=1
+
 # Comando para evitar travamentos.
 try:
     
     # Loop principal.
+    pygame.mixer.music.play(loops=-1)
     running = True
     while running:
         
@@ -87,6 +152,35 @@ try:
             # Verifica se foi fechado
             if event.type == pygame.QUIT:
                 running = False
+                
+            #verifica se apertou alguma tecla
+            if event.type == pygame.KEYDOWN:
+                #dependendo da tecla altera a vel
+                if event.key == pygame.K_LEFT:
+                    player.speedx = -8
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 8
+                    
+            #verifica se soltou alguma tecla
+            if event.type == pygame.KEYUP:
+                #dependendo da tecla altera a vel
+                if event.key == pygame.K_LEFT:
+                    player.speedx = 0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 0
+                    
+        #depois de processar os eventos
+        #atualiza a acao de cada sprite
+        all_sprites.update()
+        
+        #verifica se houve colisao entre nave e meteoro
+        hits = pygame.sprite.spritecollide(player, all_mobs, False, pygame.sprite.collide_circle)
+        if hits:
+            #toca o som da colisao
+            boom_sound.play()
+            time.sleep(1) #precisa esperar senao fecha
+            
+            running = False
     
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
